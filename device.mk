eebacb06ee788813fcad1943e75f79251b9abbaf
Michael Bestas <mkbestas@lineageos.org>
2022-02-28 22:51:41 +0200

    dubai: bootctrl: Remove bootctrl.lahaina target
     * This is no longer used after switching to QTI 1.1 impl.
    
    Change-Id: Id770b77da62c3b16eaea83cec366b299b1ea49d2

--- device.mk
+++ device.mk
@@ -59,9 +59,7 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
     android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service \
-    bootctrl.lahaina \
-    bootctrl.lahaina.recovery
+    android.hardware.boot@1.1-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
