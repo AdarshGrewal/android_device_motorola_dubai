8d6c06b46b1039cf2e2c6c480c9ee245c8ca464f
AdarshGrewal <adarshgrewal@gmail.com>
2022-10-22 12:21:34 +0530

    dubai: Import the QTI AIDL lights HAL
    Change-Id: Ie09bdd4009adad04334bb019aa431acbb4e704c4

--- device.mk
+++ device.mk
@@ -299,6 +299,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml 
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.dubai
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.dubai
